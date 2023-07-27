296ec35703a905e5d58bdd825b27c47a5a60d0d6
Lars Kutz <lars.kutz@t-online.de>
2023-07-27 21:31:44 +0200

    Create selection download format mp3, mp4

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ class App(tk.Tk):
         """ Methode to call all methods that creates the different components.
         """
         self.input_field()
+        self.download_format()
     
 
     def  input_field(self):
@@ -39,3 +40,16 @@ class App(tk.Tk):
         self.input_url = tk.Entry(self, width=71, borderwidth=3, font=("Bahnschrift 16"))
         self.input_url.insert(0, "YouTube Link... (Bsp: https://www.youtube.com/...)")
         self.input_url.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+
+    def download_format(self):
+        """ Create a LabelFrame with 2 different download formats (mp3, mp4).
+        """
+        self.select_format = tk.LabelFrame(self, text="Select Option", font=("Bahnschrift 16"))
+        self.select_format.grid(row=1, rowspan=2, column=0, padx=10, pady=10)
+        self.var_mp3 = tk.IntVar(value=1)
+        self.var_mp4 = tk.IntVar()
+        option_mp3 = tk.Checkbutton(self.select_format, text="mp3", variable=self.var_mp3, onvalue=1, offvalue=0, font=("Bahnschrift 16"))
+        option_mp3.grid(row=0, column=0, padx=5, pady=5)
+        option_mp4 = tk.Checkbutton(self.select_format, text="mp4", variable=self.var_mp4, onvalue=1, offvalue=0, font=("Bahnschrift 16"))
+        option_mp4.grid(row=0, column=1, padx=5, pady=5)
