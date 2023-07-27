b0cc3cce986e8b1cd858078bc62405d1ed6e4aa4
Lars Kutz <lars.kutz@t-online.de>
2023-07-27 21:57:26 +0200

    Add download mp3

--- yt_download.py
+++ yt_download.py
@@ -4,6 +4,7 @@
 
 from pytube import YouTube
 from pathlib import Path
+import os
 
 
 def download_mp4(url, label, folder=Path.home()):
@@ -23,3 +24,23 @@ def download_mp4(url, label, folder=Path.home()):
     except:
         return False
     
+
+def download_mp3(url, label, folder=Path.home()):
+    """ Download YouTube video in mp3-format.
+
+    Args:
+        url (str): url of the YouTube video.
+        label (tkinter.Label)
+        folder (str, optional): Path to download directory. Defaults to Path.home().
+    """
+    try:
+        video = YouTube(url)
+        label.configure(text=video.title)
+        out_path = video.streams.filter(only_audio=True).first().download(folder)
+        new_name = os.path.splitext(out_path)
+        if(os.path.exists(new_name[0] + '.mp3')):
+            os.remove(new_name[0] + '.mp3')
+        os.rename(out_path, new_name[0] + '.mp3') 
+        return True
+    except:
+        return False
